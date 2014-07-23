cbe5cfc7ac3d485185059d996afef9d5c3a7bfb5
morganacarter <morgana.carter@gmail.com>
2014-07-23 15:51:44 -0400

    Create bestof.py

---
+++ bestof.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+import csv
+BASE_URL = "http://www.washingtoncitypaper.com"
+ 
+def make_soup(url):
+html = urlopen(url).read()
+return BeautifulSoup(html, "lxml")
+ 
